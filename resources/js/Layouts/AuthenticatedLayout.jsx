81c2731615beff794a1233aad921fc235f84d76e
Higorin49 <h0815tomoki@icloud.com>
2025-01-30 15:46:03 +0000

    repair

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -111,11 +111,16 @@ export default function Authenticated({ user, header, children }) {
                             Dashboard
                         </ResponsiveNavLink>
                     </div>
-                    <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('books')} active={route().current('books')}>
-                                    Library
-                                </NavLink>
-                            </div>
+                    <div className="pt-2 pb-3 space-y-1">
+                        <ResponsiveNavLink href={route('createisbn')} active={route().current('createisbn')}>
+                            本作成
+                        </ResponsiveNavLink>
+                    </div>
+                    <div className="pt-2 pb-3 space-y-1">
+                        <ResponsiveNavLink href={route('books')} active={route().current('books')}>
+                            library
+                        </ResponsiveNavLink>
+                    </div>
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('mylibrary')} active={route().current('mylibrary')}>
                             MY本棚
